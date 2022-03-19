18b4657c221aafe94a305568a55d982b4d251cd7
denizdemirkiranxp99 <96746079+denizdemxp99@users.noreply.github.com>
2022-03-19 09:15:28 -0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int sayi1,sayi2;
+
+cout<< "Sayi Giriniz." << endl;
+
+cout<<"Ilk Sayiyi Giriniz:"<<"!\n"; 
+cin>> sayi1>>sayi2; 
+
+
+cin >> sayi1;
+cout << endl;
+ 
+cout << "Diger Sayiyi Girinizz:"<<"!\n"; 
+cin>>sayi1>>sayi2; 
+
+cout<< "Toplam:" << sayi1+sayi2 << endl;
+cout<< "Cikarma:" << sayi1-sayi2 << endl;
+
+return 0;
+
+}
