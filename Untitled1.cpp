8f7b850813e9771b5473e61c43f036ec5b5b4716
denizdemirkiranxp99 <96746079+denizdemxp99@users.noreply.github.com>
2022-04-17 16:02:12 +0300

    Update Untitled1.cpp
    17.04.2022 Hatalar Giderilmiştir.

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
+int main ()
 
-int main()
 {
 int sayi1,sayi2;
 
-cout<< "Sayi Giriniz." << endl;
+cout<<"1. Sayiyi Girin:";
 
-cout<<"Ilk Sayiyi Giriniz:"<<"!\n"; 
-cin>> sayi1>>sayi2; 
+cin>>sayi1;
+
+cout<<"2. Sayiyi Gİirn";
+
+cin>>sayi2;
+
+cout<<sayi1+sayi2;
+
+cout<<sayi1-sayi2;
 
 
-cin >> sayi1;
-cout << endl;
- 
-cout << "Diger Sayiyi Girinizz:"<<"!\n"; 
-cin>>sayi1>>sayi2; 
 
-cout<< "Toplam:" << sayi1+sayi2 << endl;
-cout<< "Cikarma:" << sayi1-sayi2 << endl;
 
-return 0;
+ return (0);
 
 }
